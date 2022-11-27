114f37acb1e97acc800e61c7124030f4d8db6ca5
123krishna456 <118282645+123krishna456@users.noreply.github.com>
2022-11-27 17:08:50 +0530

    Update javainstall.sh

--- javainstall.sh
+++ javainstall.sh
@@ -2,5 +2,3 @@
 sudo apt update
 sudo apt-get install default-jdk
 sudo apt-get install maven
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.84/bin/apache-tomcat-8.5.84.tar.gz
-sudo tar -xzvf /opt/apche

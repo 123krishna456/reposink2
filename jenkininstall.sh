1a8275c7bb20000a62c35ec768a8216ab63099d4
123krishna456 <118282645+123krishna456@users.noreply.github.com>
2022-12-29 15:05:31 +0530

    Update jenkininstall.sh

--- jenkininstall.sh
+++ jenkininstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #Installing JENKINS
+sudo apt-get update
+sudo apt-get install default-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
